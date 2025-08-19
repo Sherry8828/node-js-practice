d51afd3b1eb409196fbdb7cc7cf4935bdbc11053
Sherry <nt45139@gmail.com>
2025-08-19 12:31:44 +0800

    2-4模組系統

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let x=require("./lib.js")
+let func=require("./lib.js")
 console.log("Hello Node.js");
-let x=3;
+let y=3;
 console.log(x);
+console.log(x.x);
+func(4,5);
---
+++ lib.js
@@ -0,0 +1,5 @@
+console.log("Hello Lib.js");
+module.exports={x:10};//true、20
+module.exports=function(n1,n2){
+    console.log(n1+n2)
+}
