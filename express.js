fd39a93261cf0fe5a77f4bb17b54e27cd676ab0d
Sherry <nt45139@gmail.com>
2025-08-19 14:44:15 +0800

    3-4 設定路由

--- express.js
+++ express.js
@@ -2,6 +2,18 @@
 const express = require('express');
 //載入Application程式
 const app = express();
+
+//設定根路徑路由
+app.get('/', (req, res) => {
+    res.send('Hello World from Express.js!');
+});
+
+
+//設定/about路由，當訪問http://localhost:3000/about時，回應一段文字
+app.get('/about', (req, res) => {
+    res.send('This is the about page.');
+});
+
 //啟動伺服器網址:http://localhost:3000
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
