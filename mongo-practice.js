a6f0cc014c7542c237b68a452f0f386d21f6e71e
Sherry Hsu <nt45139@gmail.com>
2025-08-21 14:43:20 +0000

    5-8 篩選排序資料

--- mongo-practice.js
+++ mongo-practice.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const mongo = require('mongodb');
 const uri = process.env.MONGO_URI;
 const dbName = 'NodeJs';
-console.log("test");
 const client = new mongo.MongoClient(uri);
-console.log("test2");
 async function main() {
     try {
         await client.connect();
@@ -36,10 +34,22 @@ async function main() {
         console.log("批量更新結果數量:", updateResults.modifiedCount);*/
 
         //example: 刪除資料
-        let deleteResult = await collection.deleteOne({ _id: new mongo.ObjectId("68a6e91194148e936d67f956") });
+        /*let deleteResult = await collection.deleteOne({ _id: new mongo.ObjectId("68a6e91194148e936d67f956") });
         console.log("刪除結果數量:", deleteResult.deletedCount);
         findResult = await collection.find({}).toArray();
-        console.log("刪除後查詢結果:", findResult);
+        console.log("刪除後查詢結果:", findResult);*/
+
+        //example: 查詢資料
+        let query = { age: { $lt: 30 } }; // 查詢年齡小於30的資料
+        let query1={ $or: [{ name: "test1" }, { name: "test2" }] }; // 查詢名字為 test1 或 test2 的資料
+        let query2 = { $and: [{ age: { $gt: 20 } }, { age: { $lt: 30 } }] }; // 查詢年齡大於20且小於30的資料
+        let findResult = await collection.findOne(query);
+        let sortResult = await collection.find(query2).sort({
+            age: 1 // 按年齡升序排序;-1 代表降序
+        }).toArray();
+        
+        console.log("查詢結果:", findResult);   
+        console.log("排序查詢結果:", sortResult);
 
         // 這裡可以進行資料庫操作
     } catch (err) {
